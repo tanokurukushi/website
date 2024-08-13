24b5f7508d6347898c5cddb69a6d09c750de09fc
TanokuRukushi <154442612+tanokurukushi@users.noreply.github.com>
2024-08-14 00:52:25 +0900

    feat: add footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,9 @@
+<script>
+  const year = new Date().getFullYear();
+</script>
+
+<footer class="sticky top-full px-4 py-6">
+  <p class="text-center text-xs text-neutral-500">
+    &copy; {year} TanokuRukushi. All rights reserved.
+  </p>
+</footer>
