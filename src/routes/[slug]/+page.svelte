0fe8d10112b33c3f8ed4fd900192ba9103aa70ec
TanokuRukushi <154442612+tanokurukushi@users.noreply.github.com>
2024-09-22 21:42:18 +0900

    style: change width and aspect ratio of eye-catching image in article

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -12,7 +12,7 @@
 </svelte:head>
 
 <div class="prose prose-neutral mx-auto max-w-2xl p-5">
-  <img class="rounded" src={data.eyecatch.url} alt="eyecatch" />
+  <img class="aspect-video w-full rounded object-cover" src={data.eyecatch.url} alt="eyecatch" />
   <time datetime={publishedAt} class="block text-xs text-neutral-500">
     {new Date(publishedAt).toDateString()}
   </time>
