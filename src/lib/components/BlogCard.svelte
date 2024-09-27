c5bd7ef03a21184b34681ea2efdec4503755472f
TanokuRukushi <154442612+tanokurukushi@users.noreply.github.com>
2024-09-27 23:54:53 +0900

    style: update style for blog card component

--- src/lib/components/BlogCard.svelte
+++ src/lib/components/BlogCard.svelte
@@ -6,14 +6,14 @@
   const publishedAt = post.publishedAt ?? "";
 </script>
 
-<article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
+<article class="group">
   <a href="/{post.id}">
-    <img alt="eyecatch" src={post.eyecatch.url} class="h-56 w-full object-cover" />
-    <div class="p-4 sm:p-6">
+    <img alt="eyecatch" src={post.eyecatch.url} class="aspect-[8/5] w-full rounded object-cover" />
+    <div class="p-4 sm:p-5">
       <time datetime={publishedAt} class="block text-xs text-neutral-500">
         {new Date(publishedAt).toDateString()}
       </time>
-      <h2 class="mt-0.5 text-lg">{post.title}</h2>
+      <h2 class="mt-0.5 text-lg group-hover:underline">{post.title}</h2>
       <p class="mt-2 line-clamp-3 text-sm/relaxed text-neutral-500">
         {post.description}
       </p>
